452d4efa78ebb64b713e0a7eb46ff1cad91ec99b
amitx <amitbizweb@gmail.com>
2020-07-05 10:38:09 +0530

    Updates

---
+++ precache-manifest.2d601cf35a16f9821b59ac84ecc92bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a006de99817bf12de3fd6a55e2fd33e",
+    "url": "/my-cricket-score-app/index.html"
+  },
+  {
+    "revision": "b340918fdf146df76706",
+    "url": "/my-cricket-score-app/static/css/main.29816f19.chunk.css"
+  },
+  {
+    "revision": "85507c8d512b47abebec",
+    "url": "/my-cricket-score-app/static/js/2.9457834c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-cricket-score-app/static/js/2.9457834c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b340918fdf146df76706",
+    "url": "/my-cricket-score-app/static/js/main.da1f2b19.chunk.js"
+  },
+  {
+    "revision": "9c325d3340df5a9f722e",
+    "url": "/my-cricket-score-app/static/js/runtime-main.9f20d704.js"
+  }
+]);
